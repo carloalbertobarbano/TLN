c4029d6b5f85244b4d5d66be6730c3d9b8f7c551
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2019-04-03 12:06:24 +0200

    started cky

--- yoda/yoda.py
+++ yoda/yoda.py
@@ -1,3 +1,5 @@
+import numpy as np 
+
 def parse_grammar(file):
   with open(file, 'r') as f:
     lines = f.readlines()
@@ -13,7 +15,20 @@ def parse_grammar(file):
 
 def CKY(cfg, sentence):
   heads, productions = cfg
+  words = sentence.split(' ')
+  matrix = np.empty((len(words), len(words)))
+
+  for j in range(0, len(words)):
+    matrix[j-1, j] = [head for head,production in zip(heads, productions) if production == words[j]]
+
+    for i in range(j-2, 0):
+      pass
+
+  print(matrix)
+
   return None
 
 cfg = parse_grammar('paolofrancesca.cfg')
 print(cfg)
+
+CKY(cfg, 'Paolo ama Francesca dolcemente')
