9810267ed132fbbe839c3d26d5707dc101763552
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2019-04-04 19:06:57 +0200

    added .cpp

---
+++ yoda/yoda.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int main(int argc, char **argv) {
+    std::ifstream input("paolofrancesca.cfg");
+
+    std::vector<std::string> lines;
+    for(std::string line; getline(input, line);)
+        lines.push_back(line);
+
+    lines.erase(std::remove_if(lines.begin(), lines.end(), [](std::string line){return line[0] == '#';}), lines.end());
+    for (auto line : lines)
+        std::cout << line << std::endl;
+
+    return 0;
+}
