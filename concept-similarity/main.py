87504636c4088197c87b3e182a31eb4059f9e181
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2019-05-15 12:04:17 +0200

    dist

--- concept-similarity/main.py
+++ concept-similarity/main.py
@@ -1,6 +1,7 @@
 import itertools
 import numpy as np
 from typing import List
+from pprint import pprint
 from nltk.corpus import wordnet
 from nltk.corpus.reader.wordnet import Synset
 
@@ -58,29 +59,47 @@ def find_LCS(s1: Synset, s2: Synset) -> Synset:
   #print(f"custom lcs({s1},{s2})={lcs[0]}")
   return lcs[0]
 
-def distance_to_hypernym(s: Synset, hp: Synset, min_distance=WORDNET_MAX_DEPTH, curr_distance=0):
+def distance_to_hypernym(s: Synset, hp: Synset, min_distance=np.inf, curr_distance=0):
   if s == hp:
     return curr_distance
 
-  for hypernym in s.hypernyms():
+  for hypernym in s.hypernyms()+s.instance_hypernyms():
     if hypernym == hp and curr_distance+1 < min_distance:
       min_distance = curr_distance + 1
     
-    min_distance = distance_to_hypernym(hypernym, hp, min_distance, curr_distance+1)
+    min_distance = min(min_distance, distance_to_hypernym(hypernym, hp, min_distance, curr_distance+1))
   
   return min_distance
 
+wrong_distances = 0
+
 def synsets_distance(s1: Synset, s2: Synset):
+  global wrong_distances
+
   lcs = find_LCS(s1, s2)
   if not lcs:
     return None
   
-  #print()
-  custom_dist = distance_to_hypernym(s1, lcs) + distance_to_hypernym(s2, lcs)
+  dist1 = distance_to_hypernym(s1, lcs)
+  dist2 = distance_to_hypernym(s2, lcs)
+
+  custom_dist = dist1 + dist2
   nltk_dist = s1.shortest_path_distance(s2)
 
   if custom_dist != nltk_dist:
+    wrong_distances += 1
+
+    print()
     print(f"len({s1},{s2}) = Custom dist: {custom_dist}, nltk dist: {nltk_dist}")
+    print(f"dist(s1={s1}, lcs={lcs}) = {dist1}, nltk(s1={s1}, lcs={lcs}) = {s1.shortest_path_distance(lcs)}")
+    print(f"dist(s2={s2}, lcs={lcs}) = {dist2}, nltk(s2={s1}, lcs={lcs}) = {s2.shortest_path_distance(lcs)}")
+
+    if dist1 == np.inf:
+      pprint(s1.tree(lambda s:s.hypernyms()))
+    elif dist2 == np.inf:
+      pprint(s2.tree(lambda s:s.hypernyms()))
+
+    print()
 
   return custom_dist
 
@@ -138,12 +157,16 @@ def cov(X, Y):
 def pearson_corr(X, Y):
   return cov(X, Y) / (np.std(X)*np.std(Y))
 
-def spearman_corr(X, Y, num_bins=5):
+def spearman_corr(X, Y, num_bins=-1):
   X = np.array(X)
   Y = np.array(Y)
 
   if num_bins == -1:
-    return pearson_corr(np.argsort(X), np.argsort(Y))
+    set_X = np.array(list(set(X)))
+    set_Y = np.array(list(set(Y)))
+    rank_X = {val:rank for (val, rank) in zip(set_X, np.argsort(set_X))}
+    rank_Y = {val:rank for (val, rank) in zip(set_Y, np.argsort(set_Y))}
+    return pearson_corr([rank_X[val] for val in X], [rank_Y[val] for val in Y])
   
   bins = np.linspace(0, 1, num_bins)
   return pearson_corr(np.digitize(X, bins), np.digitize(Y, bins))
@@ -196,6 +219,5 @@ if __name__ == '__main__':
   print('---------- LCH -----------')
   print('Pearson correlation: {:.3f}'.format(pearson_corr(similarities['lch'], norm_similarity)))
   print('Spearman correlation: {:.3f}'.format(spearman_corr(similarities['lch'], norm_similarity)))
-  
-  #for hypernym in w2.hypernyms():
-  #  print(hypernym)
+
+  print(f'Diverging distances: {wrong_distances}')
