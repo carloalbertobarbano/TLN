f46d4aa398b6ca597db757ff967906c9ce71181c
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2019-05-28 17:00:20 +0200

    updates

--- text-summarization/main.py
+++ text-summarization/main.py
@@ -1,4 +1,7 @@
 from typing import List
+
+import nltk
+import numpy as np
 import argparse
 
 def load_nasari(path):
@@ -25,10 +28,42 @@ def load_nasari(path):
 def load_text(path):
   with open(path, 'r') as file:
     lines = file.readlines()
-  lines = list(filter(lambda line: not line.startswith('#') and line.strip(), lines))
+  lines = list(map(lambda line: line.strip(), filter(lambda line: not line.startswith('#') and line.strip(), lines)))
   return lines
 
+def create_context(paragraphs: List[str], nasari):
+  context = []
+  for paragraph in paragraphs:
+    context.append([nasari[w] for w in paragraph if w in nasari])
+  return context
+
+def rank_paragraphs_by_keywords(keywords: List[str], paragraphs: List[str]):
+  return list(map(lambda paragraph: sum([paragraph.lower().count(key.lower()) for key in keywords]), paragraphs))
+
+def remove_stopwords(words: List[str]):
+  stopwords = nltk.corpus.stopwords.words('english')
+  return list(filter(lambda word: word.lower().replace('.', '') not in stopwords, words))
+
+def find_most_frequent_words(paragraph: str):
+  words = remove_stopwords(set(paragraph.lower().replace('.', '').split(' ')))
+  freqs = map(lambda w: (w, paragraph.lower().count(w)), words)
+  return sorted(freqs, key=lambda f: f[1], reverse=True)[:10]
+
 def summarize_text(paragraphs: List[str], compression=10):
+  title, body = paragraphs[0], paragraphs[1:]
+
+  #Keywords from title
+  print("Using title for keywords:", title)
+  keywords = remove_stopwords(title.split(' '))
+  print("Filtered keywords (no stopwords):", keywords)
+
+  ranks = rank_paragraphs_by_keywords(keywords, body)
+  print("Ranks:", ranks) 
+  print('Best paragraph:', np.argmax(ranks))
+  
+  freq_words = find_most_frequent_words(body[np.argmax(ranks)])
+  print(f'Most common words in paragraph {np.argmax(ranks)}:', freq_words)
+
   return paragraphs
 
 parser = argparse.ArgumentParser()
