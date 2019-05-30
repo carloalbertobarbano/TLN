10cc1118a824a769371c9933005095b79918605c
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2019-05-30 16:03:56 +0200

    updates

--- text-summarization/main.py
+++ text-summarization/main.py
@@ -16,24 +16,26 @@ def load_nasari(path):
 
   for vec in lines:
     babel_id = vec[0]
-    word = vec[1] #.lower()
+    word = vec[1]
     synsets = filter(lambda s: s and s.strip(), vec[2:])
     if word not in nasari:
-      #print(f"Word {word} is already in nasari!")
-      nasari[word] = {'babel_id': babel_id, 'synsets': []}
+      nasari[word] = []
 
-    nasari[word]['synsets'].append({})
+    nasari[word].append({'babel_id': babel_id, 'synsets': {}})
     synsets = list(filter(lambda synset: '_' in synset, synsets))
     for synset in synsets:
       synset_name, synset_weight = synset.split('_')
-      nasari[word]['synsets'][-1][synset_name] = float(synset_weight)
+      nasari[word][-1]['synsets'][synset_name] = float(synset_weight)
 
   return nasari
 
 def load_text(path):
   with open(path, 'r') as file:
     lines = file.readlines()
-  lines = list(map(lambda line: line.strip(), filter(lambda line: not line.startswith('#') and line.strip(), lines)))
+  lines = list(map(
+    lambda line: line.strip(), 
+    filter(lambda line: not line.startswith('#') and line.strip(), lines)
+  ))
   return lines
 
 def create_context(words: List[str], nasari):
@@ -45,9 +47,11 @@ def remove_stopwords(words: List[str]):
   return list(filter(lambda word: word.replace('.', '') not in stopwords, words))
 
 def rank_paragraphs_by_keywords(keywords: List[str], paragraphs: List[str]):
-  return list(
-    map(lambda paragraph: sum([paragraph.lower().count(key.lower()) for key in keywords]), paragraphs)
-  )
+  return list(map(
+    lambda paragraph: sum([paragraph.lower().count(key.lower()) for key in keywords]), 
+    paragraphs
+  ))
+
 def remove_punctuation(string):
   chars = '.,:;!?()”“…'
   for c in chars:
@@ -85,46 +89,74 @@ def similarity(w1, w2):
   max = 0
   intersect = 0
 
-  for v1 in w1[1]['synsets']:
-    for v2 in w2[1]['synsets']: 
-      wo, inter = weighted_overlap(v1, v2)
+  for v1 in w1[1]: #['synsets']:
+    for v2 in w2[1]: #['synsets']: 
+      wo, inter = weighted_overlap(v1['synsets'], v2['synsets'])
       wo = np.sqrt(wo)
       if wo > max:
         max = wo
         intersect = inter
   
-  if max > 0:
-    print(f'WO between {w1[0]} and {w2[0]}: {max} (intersection: {intersect})')
+  #if max > 0:
+  #  print(f'WO between {w1[0]} and {w2[0]}: {max} (intersection: {intersect})')
   return max
+
+def rank_paragraphs_by_wo(context, paragraphs: List[str], nasari):
+  ranks = []
+  for paragraph in paragraphs:
+    rank = 0
+    for w2 in paragraph.split(' '):
+      if w2.capitalize() in nasari:
+        rank += sum([similarity(w1, (w2, nasari[w2.capitalize()])) for w1 in context])
+    ranks.append(rank)
+  return ranks
+
+def word_count(paragraphs: List[str]):
+  return sum([len(p.split(' ')) for p in paragraphs])
 
 def summarize_text(paragraphs: List[str], nasari, compression=10):
   title, body = paragraphs[0], paragraphs[1:]
 
-  #Keywords from title
-  print("Using title for keywords:", title)
-  keywords = remove_stopwords(title.split(' '))
-  print("Filtered keywords (no stopwords):", keywords)
+  num_words = word_count(body)
+  target_num_words = num_words - num_words*compression/100.
 
-  ranks = rank_paragraphs_by_keywords(keywords, body)
-  print("Ranks:", ranks) 
-  print('Best paragraph:', np.argmax(ranks))
-  
-  freq_words = find_most_frequent_words(' '.join(body))
-  print(f'Most common words in body:', freq_words)
+  print(f'Total number of words: {num_words}')
+  print(f'Target number of words: {target_num_words}')
 
-  context = create_context(np.array(freq_words)[:, 0], nasari)
-  pprint(context)
-  print("Words in context:", np.array(context)[:, 0])
+  i = 0
+  while num_words > target_num_words:  
+    print(f'\n\nIteration {i}')
+    print(f'Number of paragraphs: {len(body)}')
+    freq_words = find_most_frequent_words(' '.join(body))
+    print(f'Most common words in body:', freq_words)
 
-  for i in range(len(context)):
-    for j in range(len(context)):
-      if i != j:
-        w1 = context[i]
-        w2 = context[j]
-        wo = similarity(w1, w2)
-        #if wo > 0:
-        #  print(f'WO between {w1[0]} and {w2[0]}:', weighted_overlap(w1[1]['synsets'], w2[1]['synsets']))
-  return paragraphs
+    context = create_context(np.array(freq_words)[:, 0], nasari)
+    #pprint(context)
+    print("Words in context:", np.array(context)[:, 0])
+
+    #Keywords from title
+    print("Using title for keywords:", title)
+    keywords = remove_stopwords(title.split(' '))
+    print("Filtered keywords (no stopwords):", keywords)
+
+    wo_ranks = rank_paragraphs_by_wo(context, body, nasari)
+    #keyword_ranks = rank_paragraphs_by_keywords(keywords, body)
+    keyword_ranks = rank_paragraphs_by_wo(create_context(keywords, nasari), body, nasari)
+    tot_ranks = np.array(wo_ranks) + np.array(keyword_ranks)
+
+    print("WO ranks for paragraphs: ", wo_ranks)
+    print("Keyword ranks:", keyword_ranks)
+    print('Combined ranks: ', tot_ranks)
+    print('Best paragraph:', np.argmax(tot_ranks))
+    print('Lowest paragraph:', np.argmin(tot_ranks))
+
+    #Drop paragraph with lowest combined score
+    index = np.argmin(tot_ranks)
+    del body[index]
+    num_words = word_count(body)
+    i += 1
+
+  return [title] + body
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', help='input file path', type=str)
@@ -143,4 +175,4 @@ if __name__ == '__main__':
   with open(args.o, 'w') as file:
     for paragraph in summarized_text:
       file.write(paragraph)
-      file.write('\n')
+      file.write('\n\n')
