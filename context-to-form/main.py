0cd51b1bd3f32a80d7a311ea3d55d7bf7cdef663
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2019-06-07 17:46:50 +0200

    get most relevant words for definitions

--- context-to-form/main.py
+++ context-to-form/main.py
@@ -1,5 +1,6 @@
 import nltk
 import spacy
+from nltk.corpus import wordnet
 
 nlp = spacy.load('en_core_web_sm')
 
@@ -18,26 +19,36 @@ def load_defs(path):
   return terms, definitions
 
 def find_form(definitions):
+  stopwords = nltk.corpus.stopwords.words('english')
+
   for definition in definitions:
     # 1. PoS tagging
     text = nlp(definition)
     for token in text:
-      print(token, token.dep_)
-    #text = nltk.word_tokenize(definition)
-    #tags = nltk.pos_tag(text)
-    #print(text)
+      print(f'{token}[{token.dep_}] ', end='', flush=True)
+    print()
 
     # 2. Estrarre SUBJ con relativi ADJ e OBJ
+    tags = ['nsubj', 'ROOT', 'dobj', 'pobj', 'conj']
+    relevant_words = filter(lambda token: token.dep_ in tags, text)
+    relevant_words = filter(lambda token: token.text not in stopwords, relevant_words)
+    relevant_words = list(relevant_words)
+    print(relevant_words)
+
     # 3. Ricavare SUBJ principale (es. più ricorrente, o iperonimo soggetti?)
+    
+
     # 4. Synset di iperonimo ricavato
     # 5. Per i figli dell'iperonimo ottenere gloss
     # 6. W.O. delle gloss con definizioni
     # 7. trovare il max
+    print()
   pass
 
 if __name__ == '__main__':
   nltk.download('punkt')
   nltk.download('averaged_perceptron_tagger')
+  nltk.download('wordnet')
 
   terms, definitions = load_defs('esercitazione2.tsv')
   print(f'Term: {terms[0]}, definition[0]: {definitions[0][0]}')
