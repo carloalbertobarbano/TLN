a2a54dbf3c2bb76005f247696477880056e5d847
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2019-06-07 11:56:31 +0200

    add outline

---
+++ context-to-form/main.py
@@ -0,0 +1,7 @@
+# 1. PoS tagging 
+# 2. Estrarre SUBJ con relativi ADJ e OBJ
+# 3. Ricavare SUBJ principale (es. più ricorrente, o iperonimo soggetti?)
+# 4. Synset di iperonimo ricavato
+# 5. Per i figli dell'iperonimo ottenere gloss
+# 6. W.O. delle gloss con definizioni
+# 7. trovare il max
