d77e0fa12bd5fd15debda888f35c9b2683dce965
st148705 <valentino.dicianni@edu.unito.it>
2019-06-09 15:51:05 +0200

    vale_version implemented

--- content-to-form/main.py
+++ content-to-form/main.py
@@ -1,12 +1,14 @@
 import nltk
 import spacy
 import spacy_wordnet
+import collections
 
 from spacy_wordnet.wordnet_annotator import WordnetAnnotator
 from collections import Counter
-#from nltk.corpus import wordnet
 
-nlp = spacy.load('en')
+from nltk.corpus import wordnet as wn
+
+nlp = spacy.load("en")
 nlp.add_pipe(WordnetAnnotator(nlp.lang), after='tagger')
 
 def load_defs(path):
@@ -20,7 +22,6 @@ def load_defs(path):
     chunks = line.lower().strip().split('\t')
     terms.append(chunks[0])
     definitions.append(chunks[1:])
-  
   return terms, definitions
 
 def find_form(definitions):
@@ -41,7 +42,6 @@ def find_form(definitions):
     #print(relevant_words)
 
     # 3. Ricavare SUBJ principale (es. più ricorrente, o iperonimo soggetti?)
-    
     for token in relevant_words:
       print(f'WordNet domains for {token}: ')
       print(token._.wordnet.wordnet_domains())
@@ -54,11 +54,66 @@ def find_form(definitions):
     # 6. W.O. delle gloss con definizioni
     # 7. trovare il max
     print()
-  
   counter = Counter(domains)
   most_common = counter.most_common(10)
   print(most_common)
   return most_common[0]
+
+def find_form_vale_version(term, definitions):
+  stopwords = nltk.corpus.stopwords.words('english')
+  domains = []
+  relevant_words = []
+  subjects = []
+
+  print(f'Termine target: {term}\n')
+
+  for definition in definitions:
+    # 1. PoS tagging
+    text = nlp(definition)
+
+    # 2. Estrarre SUBJ con relativi ADJ e OBJ
+    tags = ['ROOT', 'dobj', 'pobj', 'amod']
+    pod = ['NOUN', 'ADJ']
+    filtered_text = filter(lambda token: token.dep_ in tags or token.dep_ in pod, text)
+
+    for token in filtered_text:
+      if token.dep_ == 'ROOT':
+        subjects.append(token.text)
+      else:
+        relevant_words.append(token.text)
+        # TODO: estende le parole importanti con il loro dominio
+
+
+  # 4. Synset di iperonimo ricavato ripetere finche punteggio new_hyper_score > old_hyper_score
+  old_hyper_score = 0
+  new_hyper_score = 0
+  target_subject = collections.Counter(subjects).most_common(1)[0][0]
+  subject_synset = wn.synsets(target_subject)[0]
+
+  while new_hyper_score >= old_hyper_score:
+    # TODO: forse considerare anche gli altri?
+    print(f'Soggetto: {subject_synset}\n')
+    hyponyms = subject_synset.hyponyms()
+
+    # 5. Per i figli dell'iperonimo ottenere gloss
+    gloss_hyponyms = map(lambda hyp: (hyp, hyp.definition()), hyponyms)
+    scores = []
+    for couple in gloss_hyponyms:
+      # 6. W.O. tra contesto (relevant_words) e parole della gloss
+      # TODO: da implementare/importare
+      score = compute_overlap_sim(relevant_words, couple[1])
+      scores.append(score)
+
+    # 7. trovare il max e settare old e new score
+    old_hyper_score = new_hyper_score
+    new_hyper_score = max(scores)
+    index_max = scores.index(new_hyper_score)
+    subject_synset = hyponyms[index_max]
+
+
+
+def compute_overlap_sim(context, text):
+  return 0
 
 if __name__ == '__main__':
   nltk.download('punkt')
@@ -66,12 +121,13 @@ if __name__ == '__main__':
   nltk.download('wordnet')
 
   terms, definitions = load_defs('esercitazione2.tsv')
-  print(f'Term: {terms[0]}, definition[0]: {definitions[0][0]}')
+  # print(f'Term: {terms[0]}, definition[0]: {definitions[0][0]}')
+  #TODO: per ogni coppia termine/definizioni
+  find_form_vale_version(terms[1], definitions[1])
+  # forms = []
+  # for definition in definitions:
+  # forms.append(find_form_vale_version(definition))
 
-  forms = []
-  for definition in definitions:
-    forms.append(find_form(definition))
-  
-  print('----------------')
-  for term, form in zip(terms, forms):
-    print(f'Ground: {term} - found: {form[0]}')
+  # print('----------------')
+  # for term, form in zip(terms, forms):
+  # print(f'Ground: {term} - found: {form[0]}')
